a992b03b140fdc4b00856c2bca7f1b9d38365d76
ismaeeladams <adamsismaeel387@gmail.com>
2022-08-16 23:48:52 +0200

    please work

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const productRoute = require("./Routes/ProductsRoute");
 const orderRoutes = require("./Routes/OrderRoute");
 // Configure Server
 const app = express(); // Initialize express as an app variable
-app.set("port", process.env.PORT || 6969); // Set the port
+app.set("port", process.env.PORT || 8080); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
 // Use individual routes when visiting these URLS
@@ -65,5 +65,5 @@ app.use(express.static("public"));
 // This is where we check URLs and Request methods to create functionality
 // GET '/' is always what will be displayed on the home page of your application
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "./" + "index.html");
+  res.sendFile(__dirname + "../" + "index.html");
 });
